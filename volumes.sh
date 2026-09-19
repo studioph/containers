cc1308ce63d647f7ca094d676f5be94a525351bc
Paul Hutchings <dev@studiop.page>
2026-09-19 17:30:57 -0400

    Specify nfsv4

--- volumes.sh
+++ volumes.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 docker volume create \
   --opt type=nfs \
   --opt device=:/mnt/storage/media \
-  --opt o=addr=truenas.g10.studiop,tcp,rw,noexec,nconnect=16 \
+  --opt o=addr=truenas.g10.studiop,tcp,rw,noexec,nconnect=16,nfsvers=4 \
   media
 
 docker volume create \
   --opt type=nfs \
   --opt device=:/mnt/storage/dwnloads \
-  --opt o=addr=qnap.studiop,tcp,rw,noexec,nconnect=16 \
+  --opt o=addr=qnap.studiop,tcp,rw,noexec,nconnect=16,nfsvers=4 \
   downloads
